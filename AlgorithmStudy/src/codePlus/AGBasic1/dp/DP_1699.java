bb76cb233193b8791d3f9f30d88dc2c1eeda3643
Minju P. <parkm.seoul@gmail.com>
2021-01-19 18:53:07 +0900

    Algorithm Study - No. 2225 (Dynamic Programming)

--- AlgorithmStudy/src/codePlus/AGBasic1/dp/DP_1699.java
+++ AlgorithmStudy/src/codePlus/AGBasic1/dp/DP_1699.java
@@ -19,11 +19,12 @@ public class DP_1699 {
 		System.out.println();
 		
 		for(int i=1; i<=n; i++) {
-//			dp[i]=i; //dp[i]는 i를 넘길 수 없음 -> why? dp[i]= (1의 제곱)*i개 일 수 있으므로
+		//dp[i]는 n을 넘길 수 없음 -> why? dp[i]= (1의 제곱)*n개 일 수 있으므로
 			for(int j=1; j*j<=i; j++) { //제곱수(j*j)는 n보다 클 수는 없다는 문제의 조건을 기억할 것
 				System.out.println("i= "+i+", j= "+j);
-				System.out.println("1. "+dp[i]);
-				System.out.println("2. "+ (dp[i-j*j]+1));
+				System.out.println("dp[i]= "+dp[i]);
+				System.out.println("dp[j]+1= "+ (dp[i-j*j]+1));
+				
 				dp[i]=Math.min(dp[i], dp[i-j*j]+1);
 				//dp[i]
 				//dp[i-1]+1, dp[i-4]+1, dp[i-9]+1,...
